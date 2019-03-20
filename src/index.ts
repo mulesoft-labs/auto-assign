63484df3cc1a5d5d366b358ed90dc30086dc398d
Guido Tamborindeguy <guido@tamborindeguy.com.ar>
2019-03-20 01:37:53 -0300

    more logging

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,10 @@ import { Handler } from './handler'
 var handler = new Handler()
 
 export = (app: Application) => {
-  app.on('pull_request.opened', async context => { handler.handlePullRequest })
+  app.log('app started')
+  app.on('*', async context => app.log(context))
+  app.on('pull_request.opened', async context => { 
+    handler.handlePullRequest 
+    app.log('pull request opened')
+  })
 }
