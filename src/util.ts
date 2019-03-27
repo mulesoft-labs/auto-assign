cf9098660a6023376e2c9459d239ef77667673c5
Guido Tamborindeguy <guido@tamborindeguy.com.ar>
2019-03-27 13:38:56 -0300

    fix

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 export function includesSkipKeywords (labels: string[], skipKeywords: string[]): boolean {
-  let lowerCaseLabels = labels.map(x => x.toLowerCase())
+  let lowerCaseLabels = labels.map(x => x.toString().toLowerCase())
   let labelsSet = new Set(lowerCaseLabels)
   for (const skipKeyword of skipKeywords) {
     if (labelsSet.has(skipKeyword.toLowerCase())) {
