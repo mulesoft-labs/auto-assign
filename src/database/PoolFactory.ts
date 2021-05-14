8f88ad6d1dd28ceed37869cf765e171d70798434
Martin D'Aloia <mdaloia@mulesoft.com>
2021-05-14 20:41:09 -0300

    Add SSL to PostgresSQL DB as required by Heroku
    Since Feb 2021 Heroku requires to use SSL connections to PostgresSQL
    databases as announced here:
    https://devcenter.heroku.com/changelog-items/2035
    
    Changed as recommended here:
    https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
    https://devcenter.heroku.com/articles/connecting-to-heroku-postgres-databases-from-outside-of-heroku#ssl

--- src/database/PoolFactory.ts
+++ src/database/PoolFactory.ts
@@ -4,7 +4,7 @@ export class PoolFactory {
     public createNewPool(): Pool {
         return new Pool({
             // pg.Client options
-            connectionString: process.env.DATABASE_URL, // e.g. postgres://user:password@host:5432/database
+            connectionString: process.env.DATABASE_URL + "?sslmode=require", // e.g. postgres://user:password@host:5432/database
 
             // pg.Pool options
             connectionTimeoutMillis: 5000,
