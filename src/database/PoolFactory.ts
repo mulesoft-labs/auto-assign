073eb1b8f41bd54785bab9383ed05f40dc7df159
Martin D'Aloia <mdaloia@mulesoft.com>
2021-05-16 19:02:38 -0300

    Revert SSL configuration in favor of PGSSLMODE=no-verify env
    To avoid the hassle of configuring SSL certificates we will set the
    PGSSLMODE=no-verify environment variable to avoid using SSL on Heroku.

--- src/database/PoolFactory.ts
+++ src/database/PoolFactory.ts
@@ -4,10 +4,7 @@ export class PoolFactory {
     public createNewPool(): Pool {
         return new Pool({
             // pg.Client options
-            connectionString: process.env.DATABASE_URL + "?sslmode=require", // e.g. postgres://user:password@host:5432/database
-            ssl: {
-                rejectUnauthorized: false,
-            },
+            connectionString: process.env.DATABASE_URL, // e.g. postgres://user:password@host:5432/database
 
             // pg.Pool options
             connectionTimeoutMillis: 5000,
